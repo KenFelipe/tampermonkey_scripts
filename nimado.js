57b379cb5730759cd999b9756ebe069a348fdacd
Felipe Kenzo <Kenzo12152@gmail.com>
2021-08-17 22:47:58 -0300

    add nimado feature

--- nimado.js
+++ nimado.js
@@ -13,6 +13,8 @@
   'use strict';
 
   // Your code here...
+  const mainHead = document.getElementsByTagName('head')[0];
+
   const css = {
     noDisplay : 'display: none !important;',
     noMargin  : 'margin : 0    !important;',
@@ -23,33 +25,76 @@
   css.noSpace = css.noMargin + css.noPadding;
   css.fullSize = css.fullWidth + css.fullHeight;
 
+  const init = () => {
+    const nimadoCss = `
+      .nimado-button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+
+        width: 22px;
+        height: 30px;
+        margin-top: 6px;
+        padding: 6px;
+
+        font-size: 2.0rem;
+        border: 1px solid #eee;
+        border-radius: 50%;
+
+        color: #eee;
+        background: transparent;
+        opacity: 1.0;
+
+        transition: all .7s ease;
+      }
+
+      .nimado-button:hover, 
+      .nimado-button.nimado-active {
+        color: #222;
+        background: #eee;
+      }
+
+      .nimado-disabled {
+        opacity: 0 !important;
+        pointer-events: none;
+      }
+
+      #nimado-wrapper {
+        display: flex;
+        flex-direction: column-reverse;
+
+        position: fixed;
+        bottom: 10px;
+        right: 0px;
+        z-index: 10000;
+      }
+
+      #nimado-wrapper.nimado-active {
+        right: 10px;
+      }
+    `;
+
+    const style = document.createElement('style');
+    style.appendChild(document.createTextNode(nimadoCss));
+
+    mainHead.appendChild(style);
+  };
+
+  const formWrapper = () => {
+    const wrapper = document.createElement('div');
+    wrapper.id = 'nimado-wrapper'
+
+    document.querySelectorAll('ytd-app')[0].appendChild(wrapper);
+  }
+
+  // Button to Adjust Window
   const formAdjustButton = () => {
     const label = '窓';
 
     const button = document.createElement('a');
     button.innerText = label;
-
-    button.style = `
-      display: block;
-      cursor: pointer;
-
-      position: fixed;
-      bottom: 10px;
-      right: 0px;
-      z-index: 10000;
-
-      width: 20px:
-      height: 20px;
-      padding: 6px;
-
-      font-size: 2.0rem;
-      border: 1px solid #eee;
-      border-radius: 50%;
-
-      color: #eee;
-      background: transparent;
-      transition: all .7s ease;
-    `;
+    button.classList.add('nimado-button');
 
     const adjustMainFrame = () => {
       const mainFrameCss = `
@@ -87,12 +132,11 @@
         #show-hide-button { ${css.noDisplay} }
       `;
 
-      const mainStyle = document.createElement('style');
-      mainStyle.appendChild(document.createTextNode(mainFrameCss));
+      const style = document.createElement('style');
+      style.appendChild(document.createTextNode(mainFrameCss));
 
-      const mainHeadTag = document.getElementsByTagName('head')[0];
-      mainHeadTag.appendChild(mainStyle);
-    }
+      mainHead.appendChild(style);
+    };
 
     const adjustChatFrame = () => {
       if (!document.getElementById('chatframe')) {
@@ -138,7 +182,7 @@
 
       const chatHead = chatFrame.getElementsByTagName('head')[0];
       chatHead.appendChild(chatStyle);;
-    }
+    };
 
     const tweakChatFrameSize = () => {
       const videoFrame = document.querySelector('#player.ytd-watch-flexy');
@@ -148,40 +192,69 @@
 
       const chatWindow = document.getElementsByTagName('ytd-live-chat-frame')[0];
       chatWindow.style = `height: ${desiredChatWindowSize + 2}px;`;
-    }
+    };
+
+    const enableOtherFeature = () => {
+      document.getElementById('toggle-supper-chat-log').classList.remove('nimado-disabled');
+    };
 
     button.addEventListener('click', () => {
       adjustChatFrame();
       adjustMainFrame();
       tweakChatFrameSize();
-      button.style.right = '10px';
+      enableOtherFeature();
+      document.getElementById('nimado-wrapper').classList.add('nimado-active')
     });
 
-    button.addEventListener('mouseout', function() {
-      this.style.color = '#eee';
-      this.style.background = 'transparent';
-    });
-
-    button.addEventListener('mouseover', function() {
-      this.style.color = '#222';
-      this.style.background = '#eee';
-    });
-
-    document.querySelectorAll('ytd-app')[0].appendChild(button);
+    document.getElementById('nimado-wrapper').appendChild(button)
   };
 
 
   // Toggle SuperChat History
+  const toggleSuperChatHistory = () => {
+    const label = 'S';
+
+    const toggle = document.createElement('a');
+    toggle.innerText = label;
+    toggle.id = 'toggle-supper-chat-log'
+
+    toggle.classList.add('nimado-button');
+    toggle.classList.add('nimado-toggle');
+    toggle.classList.add('nimado-active');
+    toggle.classList.add('nimado-disabled');
+
+    toggle.addEventListener('click', () => {
+      if (!document.getElementById('chatframe')) {
+        alert('no chatframe.');
+        return;
+      }
+
+      const chatFrame = document.getElementById('chatframe').contentWindow.document;;
+      const superChatLog = chatFrame.getElementById('ticker');
+
+      if(toggle.classList.contains('nimado-active')) {
+        toggle.classList.remove('nimado-active');
+        superChatLog.style = `${css.noDisplay}`;
+      } else {
+        toggle.classList.add('nimado-active');
+        superChatLog.style = '';
+      }
+    });
+
+    document.getElementById('nimado-wrapper').appendChild(toggle)
+  }
 
 
-
-  // Toggle SuperChat Send
+  // Toggle Send Chat
 
 
 
   //
   //
-  formAdjustButton()
+  init();
+  formWrapper();
+  formAdjustButton();
+  toggleSuperChatHistory();
   //
   //
 
